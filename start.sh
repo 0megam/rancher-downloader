d68212cd62ed177e37322959718427f4c11f837d
OmegaRus <omega@nimas.li>
2017-10-26 18:12:10 +0300

    fix

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 images_to_download=(rancher/server:v1.6.10 \
 		    rancher/agent:v1.2.6 \
                     rancher/lb-service-haproxy:v0.7.9 \
-                    gcr.io/google_containers/pause-amd64:3.0
-		    gcr.io/google_containers/kubernetes-dashboard-amd64:v1.6.1
-                    gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.2
-                    gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.2
-                    gcr.io/google_containers/k8s-dns-sidecar-amd64:1.14.2
-                    gcr.io/google_containers/heapster-influxdb-amd64:v1.1.1
-                    gcr.io/google_containers/heapster-grafana-amd64:v4.0.2
-                    gcr.io/google_containers/heapster-amd64:v1.3.0-beta.1
+                    gcr.io/google_containers/pause-amd64:3.0 \
+		    gcr.io/google_containers/kubernetes-dashboard-amd64:v1.6.1 \
+                    gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.2 \
+                    gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.2 \
+                    gcr.io/google_containers/k8s-dns-sidecar-amd64:1.14.2 \
+                    gcr.io/google_containers/heapster-influxdb-amd64:v1.1.1 \
+                    gcr.io/google_containers/heapster-grafana-amd64:v4.0.2 \
+                    gcr.io/google_containers/heapster-amd64:v1.3.0-beta.1 \
                     gcr.io/kubernetes-helm/tiller:v2.3.0)
 catalog_dir="rancher-catalog"
 subcatalog_dir="infra-templates"
